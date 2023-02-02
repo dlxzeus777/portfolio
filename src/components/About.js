b70fbd83389fb6b0f98d48a1ea44c5d74219e16a
dlxzeus777 <silverchannelcontact@gmail.com>
2023-02-02 23:35:05 +0100

    intersectionobserver api fun

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,19 @@
 import { Badge } from "@chakra-ui/react"
 import { RoughNotation } from "react-rough-notation"
+import { useRef, useEffect, useState } from "react"
 
 const About = () => {
+
+    const myRef = useRef()
+    const [isVisible, setIsVisible] = useState();
+
+    useEffect(() => {
+        const observer = new IntersectionObserver((entries) => {
+            const entry = entries[0]
+            setIsVisible(entry.isIntersecting)
+        })
+        observer.observe(myRef.current)
+    }, [])
     return (
         <section id="about">
             <div className="wrapper">
@@ -9,9 +21,14 @@ const About = () => {
                 <div className="about-containers">
                     <div className="get-to-know-me">
                         <h3 className="about-h3">Get to know me!</h3>
-                        <RoughNotation type="bracket" color="#319795" brackets={['left', 'right']} show={true} strokeWidth='3px' animationDelay={4000} animationDuration={2500}><p>Nice to meet you my name is Laci and I am a highly ambitious, self-motivated, and driven Front-End Developer based in Hungary.</p>
-                            <p>I have a wide range of hobbies and passions that keep me busy. From reading, playing sports, traveling, to working out, I am always seeking new experiences and love to keep myself engaged and learning new things.</p>
-                            <p>I believe that you should never stop growing and that's what I strive to do, I have a passion for technology and a desire to always push the limits of what is possible. I am excited to see where my career takes me and am always open to new opportunities.</p></RoughNotation>
+                        <div ref={myRef}>{isVisible
+                            ? <RoughNotation type="bracket" color="#319795" brackets={['left', 'right']} show={true} strokeWidth='3px' animationDelay={2000} animationDuration={2500}><p>Nice to meet you my name is Laci and I am a highly ambitious, self-motivated, and driven Front-End Developer based in Hungary.</p>
+                                <p>I have a wide range of hobbies and passions that keep me busy. From reading, playing sports, traveling, to working out, I am always seeking new experiences and love to keep myself engaged and learning new things.</p>
+                                <p>I believe that you should never stop growing and that's what I strive to do, I have a passion for technology and a desire to always push the limits of what is possible. I am excited to see where my career takes me and am always open to new opportunities.</p></RoughNotation>
+                                
+                            : <><p>Nice to meet you my name is Laci and I am a highly ambitious, self-motivated, and driven Front-End Developer based in Hungary.</p>
+                                <p>I have a wide range of hobbies and passions that keep me busy. From reading, playing sports, traveling, to working out, I am always seeking new experiences and love to keep myself engaged and learning new things.</p>
+                                <p>I believe that you should never stop growing and that's what I strive to do, I have a passion for technology and a desire to always push the limits of what is possible. I am excited to see where my career takes me and am always open to new opportunities.</p></>}</div>
                     </div>
                     <div className="my-skills">
                         <h3 className="about-h3">My Skills</h3>
--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -1,8 +1,23 @@
+import { useRef, useEffect, useState } from 'react';
 import profilePic from '../assets/pfp.jpg'
 import { Button } from '@chakra-ui/react'
 import { AiOutlineArrowDown } from 'react-icons/ai'
 import { RoughNotation } from "react-rough-notation";
 const Introduction = () => {
+
+    const myRef = useRef()
+    const [isVisible, setIsVisible] = useState();
+
+    useEffect(() =>
+    {
+        const observer = new IntersectionObserver((entries) =>
+        {
+            const entry = entries[0]
+            setIsVisible(entry.isIntersecting)
+        })
+        observer.observe(myRef.current)
+    }, [])
+    
     return (
         <section className='intro-section' id='home'>
             <div className="wrapper intro">
@@ -10,7 +25,7 @@ const Introduction = () => {
                     <img src={profilePic} className='profile-pic' />
                     <div className='introduction-text'>
                         <h1 className='intro-h1'>Hi, I'm Laci!</h1>
-                        <p className='intro-p'>I am a <RoughNotation type='highlight' show={true} animationDelay={1000}><span className='teal'>Front-end developer</span></RoughNotation> based in Hungary. I'm passionate about building accessible web apps or websites that users love!</p>
+                        <p className='intro-p'>I am a <span ref={myRef}>{isVisible ? <RoughNotation type='highlight' show={true} animationDelay={1000}><span className='teal'>Front-end developer</span></RoughNotation> : <span className='teal'>Front-end developer</span>}</span> based in Hungary. I'm passionate about building accessible web apps or websites that users love!</p>
                         <Button colorScheme='teal' variant='solid' className='projects-btn'><a href='#projects'>Projects</a></Button>
                     </div>
                 </div>
